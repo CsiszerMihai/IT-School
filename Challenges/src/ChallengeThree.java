466b724bfc40b39e717c99f4c9063458abb6c20b
Mihai <mihaialexandru1329@gmail.com>
2024-06-08 19:13:03 +0300

    First Session Challenges were updated

---
+++ Challenges/src/ChallengeThree.java
@@ -0,0 +1,16 @@
+public class ChallengeThree {
+
+    public static void main(String[] args) {
+
+        int numberOne = 2;
+        int numberTwo = 2;
+
+        System.out.println(2+3);
+        System.out.println(4-5);
+        System.out.println(7*13);
+        System.out.println(200/5);
+        System.out.println(13%4);
+        System.out.println(++numberOne);
+        System.out.println(--numberTwo);
+    }
+}
