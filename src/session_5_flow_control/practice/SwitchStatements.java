c1dc71783211a3ed443a017e5aaad751761c6196
Mihai <mihaialexandru1329@gmail.com>
2024-06-17 21:17:17 +0300

    5th session

--- src/session_5_flow_control/practice/SwitchStatements.java
+++ src/session_5_flow_control/practice/SwitchStatements.java
@@ -1,21 +1,19 @@
 package session_5_flow_control.practice;
 
-import java.util.Scanner;
-
 public class SwitchStatements {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String grade = scanner.nextInt();
+
+        char grade = 'A';
 
         switch (grade) {
-            case "A":
+            case 'A':
                 System.out.println("Excellent");
                 break;
-            case "B":
+            case 'B':
                 System.out.println("Good");
                 break;
-            case "C":
+            case 'C':
                 System.out.println("Average");
                 break;
             default:
