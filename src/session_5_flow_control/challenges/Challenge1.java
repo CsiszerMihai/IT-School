98d47747e4b1f1b9a799d638b4b0ca876d98b647
Csiszer Mihai <mihaialexandru1329@gmail.com>
2024-06-19 08:51:24 -0700

    Session 5 challenges updated

---
+++ src/session_5_flow_control/challenges/Challenge1.java
@@ -0,0 +1,18 @@
+package session_5_flow_control.challenges;
+
+import java.util.Scanner;
+
+public class Challenge1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int nextNumber = sc.nextInt();
+
+        if(nextNumber % 2 == 0){
+            System.out.println("number is even");
+        } else {
+            System.out.println("number is odd");
+        }
+    }
+}
