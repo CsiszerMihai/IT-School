81f9084762c55051de4b745342e45f7e5d514a97
Csiszer Mihai <mihaialexandru1329@gmail.com>
2024-06-30 12:53:08 +0300

    Session 8

---
+++ src/session_8_recap/practice/ForPractice.java
@@ -0,0 +1,17 @@
+package session_8_recap.practice;
+
+public class ForPractice {
+
+    public static void main(String[] args) {
+        int[] myArray = {1, 2, 6, 8};
+        for (int number : myArray) {
+            System.out.println(number);
+        }
+
+        System.out.println("Array length " + myArray.length);
+
+        for (int index = 0; index < myArray.length; index++) {
+            System.out.println(myArray[index]);
+        }
+    }
+}
