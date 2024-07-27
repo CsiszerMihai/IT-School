98b18e10bbd2e88f3f2da95d9d8ac77a4c617e32
Csiszer Mihai <mihaialexandru1329@gmail.com>
2024-07-27 20:17:24 +0300

    Challenge update

---
+++ src/session_10_oop_concepts/challenges/challenge2/Adress.java
@@ -0,0 +1,13 @@
+package session_10_oop_concepts.challenges.challenge2;
+
+public class Adress {
+    private String street;
+    private String city;
+    private String country;
+    private String county;
+    private int streetNumber;
+    private int buildingNumber;
+    private int floorNumber;
+    private int apartmentNumber;
+    private long postCode;
+}
